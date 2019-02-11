b4673a4dbcda6e9d7eabf72c46c4fae442b5c30e
RajenderDandyal <rajender0184@gmail.com>
2019-02-11 19:58:50 +0530

    updatted sliderCeleb index file

--- src/sliderCeleb/index.js
+++ src/sliderCeleb/index.js
@@ -1,29 +1,8 @@
 import React, {Component} from 'react'
 import styles from "../styles.module.css";
-import Badshah from "../assets/celeb/Badshah.png"
-import Deepika from "../assets/celeb/Deepika.png"
-import DiljitSingh from "../assets/celeb/Diljit-Singh.png"
-import Ramdev from "../assets/celeb/Ramdev.png"
-import RanveerSingh from "../assets/celeb/ranveer-singh.png"
-import SRK from "../assets/celeb/SRK.png"
-import Sunny from "../assets/celeb/Sunny.png"
-import Virat from "../assets/celeb/virat.png"
 
 export default class SliderCeleb extends Component {
-  state = {
-    images: [
-      DiljitSingh,
-      Ramdev,
-      RanveerSingh,
-      SRK,
-      Sunny,
-      Virat,
-      Badshah,
-      Deepika,
-    ],
-    currentIndex: 0,
-    translateValue: 0
-  };
+
   handleSelectedCelebrity = (celeb,i) => {
     this.props.handleSelectedCelebrity(celeb,i)
   };
