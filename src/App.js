249e0a3d533aea125074eca520dca39170000fa7
cpugirl <pritam.kumar@binbill.com>
2019-02-12 09:38:59 +0530

    new page added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import styles from "./styles.module.css";
 import logo from "./assets/logo.png";
 import SliderCeleb from "./sliderCeleb";
-import cloneDeep from "lodash/cloneDeep"
+import cloneDeep from "lodash/cloneDeep";
 import isEmpty from "lodash/isEmpty";
-import Badshah from "./assets/celeb/Badshah.png"
-import Deepika from "./assets/celeb/Deepika.png"
-import DiljitSingh from "./assets/celeb/Diljit-Singh.png"
-import Ramdev from "./assets/celeb/Ramdev.png"
-import RanveerSingh from "./assets/celeb/ranveer-singh.png"
-import SRK from "./assets/celeb/SRK.png"
-import Sunny from "./assets/celeb/Sunny.png"
-import Virat from "./assets/celeb/virat.png"
-import AppleXs from "./assets/phones/Apple-iPhone-Xs-Max.png"
-import HuaweiP20 from "./assets/phones/Huawei-P20-Pro.png"
-import MicromaxInfinity from "./assets/phones/Micromax-Infinity-N11.png"
-import MotoZ2 from "./assets/phones/Moto-Z2-Force.png"
-import OppoF3 from "./assets/phones/Oppo-F3-Plus.png"
-import SamgsungA9 from "./assets/phones/Samsung-Galaxy-A9-Pro.png"
-import SamgsungS9 from "./assets/phones/Samsung-Galaxy-S9+.png"
-import Xiaomi from "./assets/phones/xiaomi-mi-mix-3.png"
+import Badshah from "./assets/celeb/Badshah.png";
+import Deepika from "./assets/celeb/Deepika.png";
+import DiljitSingh from "./assets/celeb/Diljit-Singh.png";
+import Ramdev from "./assets/celeb/Ramdev.png";
+import RanveerSingh from "./assets/celeb/ranveer-singh.png";
+import SRK from "./assets/celeb/SRK.png";
+import Sunny from "./assets/celeb/Sunny.png";
+import Virat from "./assets/celeb/virat.png";
+import AppleXs from "./assets/phones/Apple-iPhone-Xs-Max.png";
+import HuaweiP20 from "./assets/phones/Huawei-P20-Pro.png";
+import MicromaxInfinity from "./assets/phones/Micromax-Infinity-N11.png";
+import MotoZ2 from "./assets/phones/Moto-Z2-Force.png";
+import OppoF3 from "./assets/phones/Oppo-F3-Plus.png";
+import SamgsungA9 from "./assets/phones/Samsung-Galaxy-A9-Pro.png";
+import SamgsungS9 from "./assets/phones/Samsung-Galaxy-S9+.png";
+import Xiaomi from "./assets/phones/xiaomi-mi-mix-3.png";
+import fb from "./assets/fb.png";
+import insta from "./assets/insta.png";
+import twitter from "./assets/twitter.png";
+import amazon from "./assets/amazon.jpg";
 import SliderPhone from "./phoneSlider";
 
 class App extends Component {
   state = {
     finallySubmit: false,
+    showResult: false,
     answerCheck: [],
     celebImages: [
       {
@@ -41,7 +46,7 @@ class App extends Component {
         match: null,
         clicked: null,
         key: 1,
-        hint: 'Swadeshi Apnao. Desh Bachao'
+        hint: "Swadeshi Apnao. Desh Bachao"
       },
       {
         image: RanveerSingh,
@@ -49,8 +54,7 @@ class App extends Component {
         match: null,
         clicked: null,
         key: 2,
-        hint: "Unlimited energy. Never drains out",
-
+        hint: "Unlimited energy. Never drains out"
       },
       {
         image: SRK,
@@ -91,7 +95,7 @@ class App extends Component {
         clicked: null,
         key: 7,
         hint: "2 achhi selfies ki keemat tum kya jaano, Ramesh babu"
-      },
+      }
     ],
     selectedCeleb: {},
     phoneImages: [
@@ -166,8 +170,7 @@ class App extends Component {
         clicked: null,
         key: 0,
         subTitle: "6.39 inches Screen"
-      },
-
+      }
     ],
     selectedPhone: null
   };
@@ -175,10 +178,12 @@ class App extends Component {
     let updatedStateImages = cloneDeep(this.state.celebImages);
     updatedStateImages.forEach(item => {
       if (item.clicked) {
-        item.clicked = null
+        item.clicked = null;
       }
     });
-    let updatedSelectedCeleb = updatedStateImages.filter(item => item.key === i);
+    let updatedSelectedCeleb = updatedStateImages.filter(
+      item => item.key === i
+    );
     console.log(updatedSelectedCeleb, celeb, i);
     let updatedSelectedCelebObj = {};
     updatedSelectedCeleb.forEach(item => {
@@ -187,38 +192,39 @@ class App extends Component {
       updatedSelectedCelebObj.match = celeb.match;
       updatedSelectedCelebObj.name = item.name;
       updatedSelectedCelebObj.key = celeb.key;
-      updatedSelectedCelebObj.hint = item.hint
+      updatedSelectedCelebObj.hint = item.hint;
     });
 
     updatedStateImages[i] = updatedSelectedCelebObj;
-    this.setState({selectedCeleb: updatedSelectedCelebObj})
-    this.setState({celebImages: updatedStateImages}, () => this.addSelectedPhoneFlag())
-
+    this.setState({ selectedCeleb: updatedSelectedCelebObj });
+    this.setState({ celebImages: updatedStateImages }, () =>
+      this.addSelectedPhoneFlag()
+    );
   };
   addSelectedPhoneFlag = () => {
     let updatedPhoneImages = cloneDeep(this.state.phoneImages);
     updatedPhoneImages.forEach(item => {
       if (!isEmpty(this.state.selectedPhone)) {
         if (item.key === this.state.selectedPhone.key) {
-          item.finallySelected = true
+          item.finallySelected = true;
         }
       }
     });
-    this.setState({phoneImages: updatedPhoneImages});
-
-  }
+    this.setState({ phoneImages: updatedPhoneImages });
+  };
   handleSelectedPhone = (phone, i) => {
     let updatedPhoneImages = cloneDeep(this.state.phoneImages);
     updatedPhoneImages.forEach(item => {
       if (!phone.finallySelected) {
         if (item.clicked) {
-          item.clicked = null
+          item.clicked = null;
         }
       }
     });
 
-
-    let updatedSelectedPhone = updatedPhoneImages.filter(item => item.key === phone.key);
+    let updatedSelectedPhone = updatedPhoneImages.filter(
+      item => item.key === phone.key
+    );
     console.log(updatedSelectedPhone, phone, i);
     let updatedSelectedPhoneObj = {};
     updatedSelectedPhone.forEach(item => {
@@ -228,98 +234,177 @@ class App extends Component {
       updatedSelectedPhoneObj.name = item.name;
       updatedSelectedPhoneObj.key = phone.key;
       updatedSelectedPhoneObj.subTitle = item.subTitle;
-      updatedSelectedPhoneObj.finallySelected = phone.finallySelected
+      updatedSelectedPhoneObj.finallySelected = phone.finallySelected;
     });
 
     updatedPhoneImages[i] = updatedSelectedPhoneObj;
 
-    this.setState({phoneImages: updatedPhoneImages}, () => this.addSelectedPhoneForCeleb());
-    this.setState({selectedPhone: updatedSelectedPhoneObj})
+    this.setState({ phoneImages: updatedPhoneImages }, () =>
+      this.addSelectedPhoneForCeleb()
+    );
+    this.setState({ selectedPhone: updatedSelectedPhoneObj });
   };
   addSelectedPhoneForCeleb = () => {
     let updatedSelectedCeleb = cloneDeep(this.state.celebImages);
     updatedSelectedCeleb.forEach(item => {
       if (item.key === this.state.selectedCeleb.key) {
-        item.match = this.state.selectedPhone
+        item.match = this.state.selectedPhone;
       }
     });
-    this.setState({celebImages: updatedSelectedCeleb}, () => this.finallySubmit())
+    this.setState({ celebImages: updatedSelectedCeleb }, () =>
+      this.finallySubmit()
+    );
   };
   finallySubmit = () => {
     if (!this.state.finallySubmit) {
       this.state.celebImages.forEach(item => {
         if (!isEmpty(item.match)) {
-          this.setState({finallySubmit: true})
+          this.setState({ finallySubmit: true });
         } else {
-          this.setState({finallySubmit: false})
+          this.setState({ finallySubmit: false });
         }
-      })
+      });
     }
   };
   handleSubmit = () => {
     if (this.state.finallySubmit) {
-      this.checkCorrectAnswers()
+      this.checkCorrectAnswers();
     } else {
-      alert("Match all celebrities with at least one phone")
+      alert("Match all celebrities with at least one phone");
     }
   };
   checkCorrectAnswers = () => {
     let answerCheck = [];
     this.state.phoneImages.forEach(item => {
-      if (item.key === item.match.key){
-        answerCheck.push(true)
+      if (item.key === item.match.key) {
+        answerCheck.push(true);
       }
-    })
-    this.setState({answerCheck})
+    });
+    this.setState({ answerCheck, showResult: true });
   };
 
   render() {
     console.log(this.state);
     return (
-        <div className={styles.body}>
-          <div className={styles.container}>
-            <div className={styles.logo}>
-              <img src={logo} alt="logo"/>
-            </div>
-            <div>Presents</div>
-            <div className={styles.heading}>#CelebValentine</div>
+      <div className={styles.body}>
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <img src={logo} alt="logo" />
+          </div>
+          <div>Presents</div>
+          <div className={styles.heading}>#CelebValentine</div>
+          {!this.state.showResult && (
             <div className={styles.subheading}>
-              <p align="center">Chose the perfect smartphone gift</p>
-              <p align="center">for your favourite celebrity</p>
+              <p align="center">
+                Chose the perfect smartphone gift
+                <br />
+                for your favourite celebrity
+              </p>
             </div>
-            <SliderCeleb handleSelectedCelebrity={(celeb, i) => this.handleSelectedCelebrity(celeb, i)}
-                         images={this.state.celebImages}/>
-            <div>{isEmpty(this.state.selectedCeleb) ? null :
-                <p align="center"
-                   style={{fontWeight: "bold", padding: '0', margin: "3px"}}>{this.state.selectedCeleb.name}</p>}
+          )}
+          {!this.state.showResult && (
+            <SliderCeleb
+              handleSelectedCelebrity={(celeb, i) =>
+                this.handleSelectedCelebrity(celeb, i)
+              }
+              images={this.state.celebImages}
+            />
+          )}
+          {!this.state.showResult && (
+            <div>
+              {isEmpty(this.state.selectedCeleb) ? null : (
+                <p
+                  align="center"
+                  style={{ fontWeight: "bold", padding: "0", margin: "3px" }}
+                >
+                  {this.state.selectedCeleb.name}
+                </p>
+              )}
             </div>
-            <div>{isEmpty(this.state.selectedCeleb) ? null :
-                <p align="center" style={{padding: '0', margin: "3px"}}>Hint</p>}
+          )}
+          {!this.state.showResult && (
+            <div>
+              {isEmpty(this.state.selectedCeleb) ? null : (
+                <p
+                  align="center"
+                  style={{ padding: "0", margin: "3px", color: "orange" }}
+                >
+                  Hint
+                </p>
+              )}
             </div>
-            <div>{isEmpty(this.state.selectedCeleb) ? null :
-                <p align="center" style={{padding: '0', margin: "3px"}}>{this.state.selectedCeleb.hint}</p>}
+          )}
+          {!this.state.showResult && (
+            <div>
+              {isEmpty(this.state.selectedCeleb) ? null : (
+                <p align="center" style={{ padding: "0", margin: "3px" }}>
+                  {this.state.selectedCeleb.hint}
+                </p>
+              )}
             </div>
-            {/*<div style={{
-              display: "flex",
-              flexDirection: "row",
-              margin: '20px',
-              alignItems: 'center',
-              justifyContent: "center"
-            }}>
-              <div style={{width: "150px", height: "1px", backgroundColor: "red"}}></div>
-              <div style={{padding: "10px"}}><p align="center" style={{color: "red"}}>Slider</p></div>
-              <div style={{width: "150px", height: "1px", backgroundColor: "red"}}></div>
-            </div>*/}
-            <SliderPhone handleSelectedPhone={(phone, i) => this.handleSelectedPhone(phone, i)}
-                         images={this.state.phoneImages}/>
+          )}
+          {!this.state.showResult && (
+            <SliderPhone
+              handleSelectedPhone={(phone, i) =>
+                this.handleSelectedPhone(phone, i)
+              }
+              images={this.state.phoneImages}
+            />
+          )}
+          {!this.state.showResult && (
             <div onClick={this.handleSubmit} className={styles.buttonContainer}>
-              <div className={this.state.finallySubmit ? styles.button : styles.disableBtn}>
+              <div
+                className={
+                  this.state.finallySubmit ? styles.button : styles.disableBtn
+                }
+              >
                 Submit
               </div>
             </div>
-            <div>{this.state.answerCheck.length}</div>
-          </div>
+          )}
+
+          {this.state.showResult && (
+            <div
+              style={{
+                backgroundColor: "orange",
+                color: "white",
+                fontSize: 20,
+                padding: 3
+              }}
+            >
+              You got {this.state.answerCheck.length} out of 8 correct answers.
+            </div>
+          )}
+          {this.state.showResult && (
+            <div style={{ textAlign: "center" }}>
+              <p>
+                Share it with your friends using <br />
+                the <span style={{ fontWeight: "bold" }}>#CelebValentine</span>
+              </p>
+            </div>
+          )}
+          {this.state.showResult && (
+            <div>
+              <img style={{ padding: 5 }} src={fb} alt="facebook" />
+              <img style={{ padding: 5 }} src={insta} alt="facebook" />
+              <img style={{ padding: 5 }} src={twitter} alt="facebook" />
+            </div>
+          )}
+          {this.state.showResult && (
+            <div style={{ textAlign: "center" }}>
+              <p>
+                and stand a chance to win <br />
+                amazon vouchers.
+              </p>
+            </div>
+          )}
+          {this.state.showResult && (
+            <div style={{ textAlign: "center" }}>
+              <img src={amazon} style={{ width: "90%" }} alt="amazon" />
+            </div>
+          )}
         </div>
+      </div>
     );
   }
 }
